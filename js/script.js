ffb2bba027015bd5e5e3f50c6cfcf42ff9b0d1d7
Kayla Baca <119377119+kayharr@users.noreply.github.com>
2024-08-24 15:09:03 -0500

    created validation for character input and valid inputs stored in guessedLetters array.

--- js/script.js
+++ js/script.js
@@ -16,7 +16,9 @@ const messages = document.querySelector(".message");
 const playAgain = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
+// **symbol as a letter placeholder** //
 const placeholder = function (word) {
     const placeholderLetters = [];
     for (const letter of word) {
@@ -28,9 +30,41 @@ const placeholder = function (word) {
 
 placeholder(word);
 
+// **guess button** //
 button.addEventListener("click", function (e) {
     e.preventDefault();
-    const inputValue = letterInput.value;
-    console.log(inputValue);
-    button.value = "";
+    messages.innerText = "";
+    const guess = letterInput.value;
+    console.log(guess);
+    const goodGuess = validateInput(guess);
+   
+    if (goodGuess) {
+        makeGuess(guess);
+    }
+    letterInput.value = "";
 });
+
+// **validate player input** //
+const validateInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        messages.innerText = "Must enter a letter.";
+    } else if (input.length > 1) {
+        messages.innerText = "Please enter only one letter.";
+    } else if (!input.match(acceptedLetter)) {
+        messages.innerText = "Please enter a letter from A to Z";
+    } else {
+        return input;
+    }
+};
+
+// **adding valid inputs to array** //
+const makeGuess = function (guess) {
+ guess = guess.toUpperCase();
+ if (guessedLetters.includes(guess)) {
+    messages.innerText = "You already guessed that letter, silly! Try again.";
+ } else {
+    guessedLetters.push(guess);
+    console.log(guessedLetters);
+ }
+};
